caba61d7c108bb7e523a7997203830bfb9be5939
ads2senac <ads.senac903@gmail.com>
2020-04-15 16:18:00 -0300

    testes javascript na pagina de doacoes

---
+++ app_450ML/js/doacao.js
@@ -0,0 +1,10 @@
+//Função para agendamento da doação
+
+var botaoAgendar = document.querySelector("#agendar-doacao");
+botaoAgendar.addEventListener("click", function(){
+    event.preventDefault();
+
+    var form = document.querySelector("#agendar");
+
+    console.log(select.selectedIndex);    
+});
--- app_450ML/js/login.js
+++ app_450ML/js/login.js
@@ -1,4 +1,5 @@
-         
+//Função para troca de Login
+
 $(document).ready(function(){ 
     $(".login-usuario").hide();
     $(".login-instituicao").hide();
@@ -16,4 +17,15 @@ $(document).ready(function(){
     $(".login-instituicao").show(1500);
     $("#capa").hide();
     }); 
+});
+
+//Função para agendamento da doação
+
+var botaoAgendar = document.querySelector("#agendar-doacao");
+botaoAgendar.addEventListener("click", function(){
+    event.preventDefault();
+
+    var form = document.querySelector("#agendar");
+
+    console.log(form.section.instituicao.value);    
 });
